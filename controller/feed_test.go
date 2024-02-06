e8ab6d2e080c2aefbd2d8122ee21164c9bb2ab74
David Henning <david@webcodr.io>
2024-02-06 18:35:45 +0100

    Replace mocked tests with real API tests

--- controller/feed_test.go
+++ controller/feed_test.go
@@ -1,142 +1,133 @@
 package controller
 
 import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/steinfletcher/apitest-jsonpath"
+	"gorm.io/gorm"
 	"net/http"
-	"net/http/httptest"
-	"pcast-api/router/validator"
-	"strings"
+	"os"
+	"pcast-api/db"
+	"pcast-api/router"
+	"pcast-api/store"
 	"testing"
 
-	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-
-	"pcast-api/model"
+	"github.com/steinfletcher/apitest"
 )
 
-type MockFeedStore struct {
-	mock.Mock
+var d *gorm.DB
+
+func TestMain(m *testing.M) {
+	d = db.NewTestDB()
+	db.AutoMigrate(d)
+
+	code := m.Run()
+
+	db.RemoveTables(d)
+
+	os.Exit(code)
 }
 
-func (m *MockFeedStore) FindAll() ([]model.Feed, error) {
-	args := m.Called()
-	return args.Get(0).([]model.Feed), args.Error(1)
+func newApp() *echo.Echo {
+	r := router.New()
+	apiV1 := r.Group("/api")
+
+	feedStore := store.New(d)
+	feedController := New(feedStore)
+	feedController.Register(apiV1)
+
+	return r
 }
 
-func (m *MockFeedStore) Create(feed *model.Feed) error {
-	args := m.Called(feed)
-	return args.Error(0)
+func TestGetFeeds(t *testing.T) {
+	apitest.New().
+		Handler(newApp()).
+		Get("/api/feeds").
+		Expect(t).
+		Assert(jsonpath.Len("$", 0)).
+		Status(http.StatusOK).
+		End()
 }
 
-func (m *MockFeedStore) FindByID(id uuid.UUID) (*model.Feed, error) {
-	args := m.Called(id)
-	return args.Get(0).(*model.Feed), args.Error(1)
+func TestCreateFeed(t *testing.T) {
+	apitest.New().
+		Handler(newApp()).
+		Post("/api/feeds").
+		JSON(`{"url": "https://example.com"}`).
+		Expect(t).
+		Status(http.StatusCreated).
+		End()
+
+	apitest.New().
+		Handler(newApp()).
+		Get("/api/feeds").
+		Expect(t).
+		Assert(jsonpath.Len("$", 1)).
+		Status(http.StatusOK).
+		End()
+
+	db.TruncateTables(d)
 }
 
-func (m *MockFeedStore) Delete(feed *model.Feed) error {
-	args := m.Called(feed)
-	return args.Error(0)
+func TestCreateFeedPropertyNameError(t *testing.T) {
+	apitest.New().
+		Handler(newApp()).
+		Post("/api/feeds").
+		JSON(`{"ur": "https://example.com"}`).
+		Expect(t).
+		Status(http.StatusBadRequest).
+		End()
+
+	db.TruncateTables(d)
 }
 
-func TestGetFeedsReturnsFeeds(t *testing.T) {
-	mockStore := new(MockFeedStore)
-	mockFeed := &model.Feed{URL: "https://example.com"}
-	mockStore.On("FindAll").Return([]model.Feed{*mockFeed}, nil)
+func TestCreateFeedUrlValidationError(t *testing.T) {
+	apitest.New().
+		Handler(newApp()).
+		Post("/api/feeds").
+		JSON(`{"url": "://example.com"}`).
+		Expect(t).
+		Status(http.StatusBadRequest).
+		End()
 
-	feedController := New(mockStore)
-
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/feeds", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-
-	err := feedController.GetFeeds(c)
-
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, rec.Code)
-	assert.Equal(t, `[{"id":"00000000-0000-0000-0000-000000000000","url":"https://example.com"}]`+"\n", rec.Body.String())
-	mockStore.AssertExpectations(t)
+	db.TruncateTables(d)
 }
 
-func TestCreateFeedReturnsCreatedFeed(t *testing.T) {
-	mockStore := new(MockFeedStore)
-	mockStore.On("Create", mock.AnythingOfType("*model.Feed")).Return(nil)
+func TestDeleteFeed(t *testing.T) {
+	apitest.New().
+		Handler(newApp()).
+		Post("/api/feeds").
+		JSON(`{"url": "https://example.com"}`).
+		Expect(t).
+		Status(http.StatusCreated).
+		End()
 
-	feedController := New(mockStore)
+	apitest.New().
+		Handler(newApp()).
+		Get("/api/feeds").
+		Expect(t).
+		Assert(jsonpath.Len("$", 1)).
+		Status(http.StatusOK).
+		End()
 
-	e := echo.New()
-	e.Validator = validator.New()
-	req := httptest.NewRequest(http.MethodPost, "/feeds", strings.NewReader(`{"url":"https://example.com"}`))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	result, err := store.New(d).FindAll()
 
-	err := feedController.CreateFeed(c)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusCreated, rec.Code)
-	mockStore.AssertExpectations(t)
-}
+	apitest.New().
+		Handler(newApp()).
+		Delete(fmt.Sprintf("/api/feeds/%s", result[0].ID)).
+		Expect(t).
+		Status(http.StatusOK).
+		End()
 
-func TestCreateFeedReturnsPropertyNameValidationError(t *testing.T) {
-	mockStore := new(MockFeedStore)
-
-	feedController := New(mockStore)
-
-	e := echo.New()
-	e.Validator = validator.New()
-	req := httptest.NewRequest(http.MethodPost, "/feeds", strings.NewReader(`{"ur":"https://example.com"}`))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-
-	err := feedController.CreateFeed(c)
-
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusBadRequest, rec.Code)
-	assert.Equal(t, "\"code=400, message=Key: 'CreateFeedRequest.URL' Error:Field validation for 'URL' failed on the 'required' tag\"\n", rec.Body.String())
-	mockStore.AssertExpectations(t)
-}
-
-func TestCreateFeedReturnsUrlValidationError(t *testing.T) {
-	mockStore := new(MockFeedStore)
-
-	feedController := New(mockStore)
-
-	e := echo.New()
-	e.Validator = validator.New()
-	req := httptest.NewRequest(http.MethodPost, "/feeds", strings.NewReader(`{"url":"://example.com"}`))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-
-	err := feedController.CreateFeed(c)
-
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusBadRequest, rec.Code)
-	assert.Equal(t, "\"code=400, message=Key: 'CreateFeedRequest.URL' Error:Field validation for 'URL' failed on the 'url' tag\"\n", rec.Body.String())
-	mockStore.AssertExpectations(t)
-}
-
-func TestDeleteFeedReturnsNoContent(t *testing.T) {
-	mockStore := new(MockFeedStore)
-	mockFeed := &model.Feed{URL: "https://example.com"}
-	mockStore.On("FindByID", mock.AnythingOfType("uuid.UUID")).Return(mockFeed, nil)
-	mockStore.On("Delete", mockFeed).Return(nil)
-
-	feedController := New(mockStore)
-
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodDelete, "/feeds/3e2076e6-1c0e-4a7e-9a6e-4b8e7f8f3e84", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	c.SetParamNames("id")
-	c.SetParamValues("3e2076e6-1c0e-4a7e-9a6e-4b8e7f8f3e84")
-
-	err := feedController.DeleteFeed(c)
-
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, rec.Code)
-	mockStore.AssertExpectations(t)
+	apitest.New().
+		Handler(newApp()).
+		Get("/api/feeds").
+		Expect(t).
+		Assert(jsonpath.Len("$", 0)).
+		Status(http.StatusOK).
+		End()
 }
